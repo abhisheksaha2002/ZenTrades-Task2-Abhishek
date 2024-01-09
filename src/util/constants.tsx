af3a9c8f36d53a9f708c19d4fe15fe67ea0963db
Abhishek Saha <84782975+abhisheksaha2002@users.noreply.github.com>
2024-01-09 21:35:20 +0530

    Add files via upload

---
+++ src/util/constants.tsx
@@ -0,0 +1,65 @@
+export const file_type = [
+  {
+    name: "CSV",
+    value: "csv",
+  },
+  {
+    name: "JSON",
+    value: "json",
+  },
+];
+
+export const character_ecoding = [
+  {
+    name: "UTF-8",
+    value: "utf-8",
+  },
+  {
+    name: "UTF-16",
+    value: "utf-16",
+  },
+  {
+    name: "UTF-32",
+    value: "utf-32",
+  },
+  {
+    name: "ISO-8859-1",
+    value: "iso-8859-1",
+  },
+  {
+    name: "Windows-1252",
+    value: "windows-1252",
+  },
+];
+
+export const delimiter = [
+  {
+    name: "Comma",
+    value: ",",
+  },
+  {
+    name: "Semicolon",
+    value: ";",
+  },
+  {
+    name: "Tab",
+    value: "\t",
+  },
+  {
+    name: "Pipe",
+    value: "|",
+  },
+  {
+    name: "Space",
+    value: " ",
+  },
+];
+
+export const available_fields_data = [
+  "Product Id",
+  "SubCategory",
+  "Title",
+  "Price",
+  "Popularity",
+  "Description",
+];
